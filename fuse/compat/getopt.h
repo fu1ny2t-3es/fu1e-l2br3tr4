958105a90ad2b5825ad002ba563cc3f9f879d763
Craig <1188869+cscd98@users.noreply.github.com>
2026-09-06 14:12:18 +0100

    libretro: fix compilation new vita sdk (#183)

--- fuse/compat/getopt.h
+++ fuse/compat/getopt.h
@@ -138,7 +138,7 @@ struct option
    `getopt'.  */
 
 #if (defined __STDC__ && __STDC__) || defined __cplusplus
-# ifdef __GNU_LIBRARY__
+# if defined(__GNU_LIBRARY__) || defined(VITA)
 /* Many other libraries have conflicting prototypes for getopt, with
    differences in the consts, in stdlib.h.  To avoid compilation
    errors, only prototype getopt for the GNU C library.  */
